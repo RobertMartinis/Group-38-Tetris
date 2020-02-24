122929ba64c91cce3a8dcd85c772a0f5c105554f
Robert Martinis <57859068+RobertMartinis@users.noreply.github.com>
2020-02-24 21:02:52 +0100

    Code for rotating the block

--- rotateBlock.hs
+++ rotateBlock.hs
@@ -1,1 +1,16 @@
-undefined
+
+rotateBlock :: GameState -> GameState
+rotateBlock game = game { fallingBlock = (newBlock,color,(x,y))
+                        }
+  where
+    (block, color, (x,y)) = fallingBlock game
+    newBlock = rotateBlock' block
+    
+    rotateBlock' :: Block -> Block
+    rotateBlock' [(a1:a2:a3:a4:[]),
+                  (b1:b2:b3:b4:[]),
+	          (c1:c2:c3:c4:[]),
+	          (d1:d2:d3:d4:[])] = [[d1, c1, b1, a1],
+                                       [d2, c2, b2, a2],
+	                               [d3, c3, b3, a3],
+	                               [d4, c4, b4, a4]]
